8da9ee14cc9446180018adec3ad395a38b1517c4
jasar-ck <jasirck@gmail.com>
2024-01-28 14:46:56 +0530

    Internationalization

--- src/main/java/com/hs/hello/HelloWorldController.java
+++ src/main/java/com/hs/hello/HelloWorldController.java
@@ -1,10 +1,18 @@
 package com.hs.hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	
 	@GetMapping("/hello")
 	public String helloWorld() {
@@ -16,4 +24,9 @@ public class HelloWorldController {
 		return new UserDetails("John", "mike", "Paris");
 	}
 
+	
+	@GetMapping("/hello-in")
+	public String helloi18n(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+		return messageSource.getMessage("label.hello",null,  new Locale(locale)) ;
+	}
 }
