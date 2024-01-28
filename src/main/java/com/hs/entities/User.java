0398fb4305cd483294087a927e8ed1004097c5a3
jasar-ck <jasirck@gmail.com>
2024-01-28 15:15:24 +0530

    Added JsonIgnore and JsonIgnoreProperty

--- src/main/java/com/hs/entities/User.java
+++ src/main/java/com/hs/entities/User.java
@@ -2,6 +2,9 @@ package com.hs.entities;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,6 +18,7 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "user_data")
+@JsonIgnoreProperties({ "firstname", "lastname"})
 public class User {
 	
 	@Id
@@ -37,6 +41,7 @@ public class User {
 	@Column(name = "ROLE", length = 50, nullable = false)
 	private String role;
 	
+	@JsonIgnore
 	@Column(name = "SSN", length = 50, nullable = false, unique = true)
 	private String ssn;
 
